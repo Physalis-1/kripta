c5bb371d433f925e6feb1b96b2efca841ade86a0
Physalis-1 <84348788+Physalis-1@users.noreply.github.com>
2021-12-03 12:03:51 +0300

    Update Enc.java

--- src/Enc.java
+++ src/Enc.java
@@ -1,10 +1,14 @@
+import javax.swing.*;
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Base64;
 
 public class Enc {
-    public Enc (int index, String name_file, byte [] key, String path1, byte [] vector) throws Exception {
+    public Enc (int index, String name_file, byte [] key, String path1, byte [] vector, String ip, int port, String loginss) throws Exception {
         int k = 0;
         File file = new File(path1);
         FileOutputStream fos = new FileOutputStream(name_file);
@@ -16,6 +20,8 @@ public class Enc {
         FileInputStream inputStream = new FileInputStream(path1);
         int bufer=64000;
         long ost=file.length()%bufer;
+        int datch=0;
+        String hash="";
         while (inputStream.available()>0){
             byte[] message = new byte[64000];
             if (inputStream.available()<bufer) bufer= inputStream.available();
@@ -24,34 +30,64 @@ public class Enc {
             vector= (byte[]) text[1];
             byte [] text1= (byte[]) text[0];
             fos.write(text1);
+            if (datch<1)
+            {
+                MessageDigest md = null;
+                try {
+                    md = MessageDigest.getInstance("SHA-256");
+                    md.update(Base64.getEncoder().encodeToString(text1).getBytes(StandardCharsets.UTF_8));
+                    byte[] digest = md.digest();
+                    hash = Base64.getEncoder().encodeToString(digest);
+                } catch (NoSuchAlgorithmException ex) {
+                    ex.printStackTrace();
+                }
+                datch = datch + 1;
+            }
+
         }
         fos.close();
-
-        try(FileWriter writer = new FileWriter("key.txt", false))
-        {
-            for (int i = 0; i < (Km.length); i++){
-                writer.write(String.valueOf(Km[i]));
-                writer.write('|');
-            }
-            writer.write("!");
-            for (int i = 0; i < (Km.length); i++){
-                writer.write(String.valueOf(Kr[i]));
-                writer.write('|');
-            }
-            writer.write("!");
-            for (int i = 0; i < (vec.length); i++){
-                writer.write(String.valueOf(vec[i]));
-                writer.write('|');
-            }
-            writer.write("!");
-            writer.write(String.valueOf(ost));
-            writer.append('|');
-            writer.write("!");
-            writer.write(String.valueOf(key.length));
-            writer.append('|');
-            writer.write("!");
-            writer.flush();
+        Client client = new Client();
+        String [] massiv=new String[8];
+        massiv[0]="flag4";
+        massiv[1]=loginss;
+        massiv[2]=hash;
+        String stroka="";
+        for (int i = 0; i < (Km.length); i++){
+            stroka=stroka+String.valueOf(Km[i])+"|";
         }
+        massiv[3]=stroka;
+        stroka="";
+        for (int i = 0; i < (Km.length); i++){
+            stroka=stroka+String.valueOf(Kr[i])+"|";
+        }
+        massiv[4]=stroka;
+        stroka="";
+        for (int i = 0; i < (vec.length); i++){
+            stroka=stroka+String.valueOf(vec[i])+"|";
+        }
+        massiv[5]=stroka;
+        massiv[6]=String.valueOf(ost);
+        massiv[7]=String.valueOf(key.length);
+        System.out.println();
+        System.out.println();
+        System.out.println("/////////////////////////////////////////////////////////////");
+        for (int i = 0; i < (massiv.length); i++){
+            System.out.println(massiv[i]);
+        }
+        System.out.println("/////////////////////////////////////////////////////////////");
+        System.out.println();
+        System.out.println();
+
+        ArrayList<String> array =client.select(ip,port,massiv);
+        System.out.println(array.get(0));
+        if (array.get(0).equals("ok")) {
+            JOptionPane.showMessageDialog(null, "Данные добавлены в БД\n");
+        }
+        else if (array.get(0).equals("error"))
+        {
+            JOptionPane.showMessageDialog(null, "Данные не добавлены в БД\n возможно они уже там были");
+        }
+
     }
 }
 
