6129aba89446477498233d47c57a97ce5d3b7139
Physalis-1 <84348788+Physalis-1@users.noreply.github.com>
2021-12-03 12:04:14 +0300

    Update Dec.java

--- src/Dec.java
+++ src/Dec.java
@@ -1,10 +1,16 @@
+import javax.swing.*;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Base64;
 
 public class Dec {
-    public  Dec (String name_file, String path1,String path2 ) throws Exception {
+    public  Dec (String name_file, String path1,String path2,String ip, int port, String loginss) throws Exception {
 
         BufferedReader bufferedReader = new BufferedReader(new FileReader(path2));
         int [] m=new int[16];
@@ -17,59 +23,90 @@ public class Dec {
         int i=0;
         long ost=0;
         int key_len=0;
-        while (symbol != -1) {  // Когда дойдём до конца файла, получим '-1'
-            // Что-то делаем с прочитанным символом
-            // Преобразовать в char:
-            if ((char)symbol=='!')
-            {
-                datchik=datchik+1;
-                i=0;
-                c="";
-            }
-            else if ((char)symbol=='|')
-            {
+        byte[] text2 = new byte[64000];
+        int bufer=64000;
+        FileInputStream inputStream = new FileInputStream(path1);
+        if (inputStream.available()<bufer) bufer= inputStream.available();
+        inputStream.read(text2,0,bufer);
+        inputStream.close();
+        MessageDigest md = null;
+        String hash="";
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+            md.update(Base64.getEncoder().encodeToString(text2).getBytes(StandardCharsets.UTF_8));
+            byte[] digest = md.digest();
+            hash = Base64.getEncoder().encodeToString(digest);
+        } catch (NoSuchAlgorithmException ex) {
+            ex.printStackTrace();
+        }
+        Client client = new Client();
+        String [] massiv=new String[3];
+        massiv[0]="flag2";
+        massiv[1]=loginss;
+        massiv[2]=hash;
+        ArrayList<String> array =client.select(ip,port,massiv);
+        System.out.println(array.get(0));
+        if (array.get(0).equals("error")){
+            JOptionPane.showMessageDialog(null, "Данных нет");
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(null, "Начинаем расшифровку");
+        }
 
-                if (datchik==0)
-                {
-                    m[i]=Integer.parseInt(c.trim());
-                    i=i+1;
-                    c="";
-                }
-                else if (datchik==1)
-                {
-                    r[i]=Integer.parseInt(c.trim());
-                    i=i+1;
-                    c="";
-                }
-                else if (datchik==2)
-                {
-                    vector[i]=(byte)Integer.parseInt(c.trim());
-                    i=i+1;
-                    c="";
-                }
-                else if (datchik==3)
-                {
-                    ost=Integer.parseInt(c.trim());
-                    c="";
-                }
-                else if (datchik==4)
-                {
-                    key_len=Integer.parseInt(c.trim());
-                    c="";
-                }
+        String stro="";
+        stro=array.get(0);
+        for (int j = 0; j < stro.length(); j++) {
+            if (stro.charAt(j)=='|'){
+                m[i]=Integer.parseInt(c.trim());
+                i=i+1;
+                c="";
             }
             else
             {
-                c=c+(char)symbol;
+                c=c+stro.charAt(j);
             }
-            symbol = bufferedReader.read(); // Читаем символ
         }
+        i=0;
+        stro=array.get(1);
+        c="";
+        for (int j = 0; j < stro.length(); j++) {
+            if (stro.charAt(j)=='|'){
+                r[i]=Integer.parseInt(c.trim());
+                i=i+1;
+                c="";
+            }
+            else
+            {
+                c=c+stro.charAt(j);
+            }
+        }
+        i=0;
+        stro=array.get(2);
+        c="";
+        for (int j = 0; j < stro.length(); j++) {
+            if (stro.charAt(j)=='|'){
+                vector[i]=(byte)Integer.parseInt(c.trim());
+                i=i+1;
+                c="";
+            }
+            else
+            {
+                c=c+stro.charAt(j);
+            }
+        }
+        ost=Integer.parseInt(array.get(3).trim());
+        key_len=Integer.parseInt(array.get(4).trim());
+        System.out.println(array.get(0));
+        System.out.println(array.get(1));
+        System.out.println(array.get(2));
+        System.out.println(array.get(3));
+        System.out.println(array.get(4));
 
         Cast_dec cast = new Cast_dec(key_len);
         FileOutputStream fos = new FileOutputStream(name_file);
         byte [] vec= Arrays.copyOf(vector,8);
-        FileInputStream inputStream = new FileInputStream(path1);
-        int bufer=64000;
+        inputStream = new FileInputStream(path1);
         byte[] text = new byte[64000];
         File file = new File(path1);
         long cel;
